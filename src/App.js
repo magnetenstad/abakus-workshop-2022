967edbcd0974cf50d29cc8a9a26cfdb2676f404a
magnetenstad <tenstad.magne@gmail.com>
2022-10-12 17:02:14 +0200

    Complete Task 1
    Co-authored-by: Hans-Marius Øverås <hansmariusoveras@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 import esriConfig from '@arcgis/core/config.js';
 
-import { AppContext } from "./state/context";
-import MapComponent from "./components/Map";
-import RouteWidget from "./components/RouteWidget";
-import "./App.css";
+import { AppContext } from './state/context';
+import MapComponent from './components/Map';
+import RouteWidget from './components/RouteWidget';
+import './App.css';
 
 function App() {
   // Opprett store som sendes rundt til ulike komponenter
-  esriConfig.apiKey = "AAPK52519f141ec04565b33944a7da4bc90fnViwerekXzgz0Xmo2l0frkfDum-JLaO1qlOfNgp6QFA4tUSOS_ZEur7zjsuqLFJu";
+  esriConfig.apiKey =
+    'AAPK52519f141ec04565b33944a7da4bc90fnViwerekXzgz0Xmo2l0frkfDum-JLaO1qlOfNgp6QFA4tUSOS_ZEur7zjsuqLFJu';
   const [mapView, setMapView] = useState(null);
   const [featureLayer, setFeatureLayer] = useState(null);
   const [point, setPoint] = useState({
-    type: "point",
+    type: 'point',
     latitude: 63.4305,
-    longitude: 10.4500
+    longitude: 10.45,
   });
 
   const store = {
     mapView: { value: mapView, set: setMapView },
     featureLayer: { value: featureLayer, set: setFeatureLayer },
     point: { value: point, set: setPoint },
-  }
+  };
 
   return (
     <AppContext.Provider value={store}>
-      <div style={{ height: "100%", width: "100%" }}>
+      <div style={{ height: '100%', width: '100%' }}>
         <MapComponent />
-        {/* <RouteWidget /> */}
+        {/*<RouteWidget />*/}
       </div>
     </AppContext.Provider>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,17 @@
-import React, { useRef, useContext, useEffect } from "react";
-import { AppContext } from "../state/context";
+import React, { useRef, useContext, useEffect } from 'react';
+import { AppContext } from '../state/context';
 
-import esriConfig from "@arcgis/core/config.js";
-import MapView from "@arcgis/core/views/MapView";
-import Map from "@arcgis/core/Map";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import PopupTemplate from "@arcgis/core/PopupTemplate";
+import esriConfig from '@arcgis/core/config.js';
+import MapView from '@arcgis/core/views/MapView';
+import Map from '@arcgis/core/Map';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import PopupTemplate from '@arcgis/core/PopupTemplate';
 
-import "../App.css";
+import '../App.css';
 
 const MapComponent = () => {
   // Required: Set this property to insure assets resolve correctly.
-  esriConfig.assetsPath = "./assets";
+  esriConfig.assetsPath = './assets';
   const mapDiv = useRef(null);
   const context = useContext(AppContext);
 
@@ -23,7 +23,7 @@ const MapComponent = () => {
       // En liste med valg finner vi i API dokumentasjonen:
       // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
       const map = new Map({
-        basemap: "",
+        basemap: 'arcgis-streets-night',
       });
 
       // Vi ønsker så å hente data som vi kan legge til i kartet.
@@ -34,16 +34,28 @@ const MapComponent = () => {
       // Om man ønsker å ha en popup når man trykker på punktene i FeatureLayeret trenger man også å legge
       // inn en popup template i featurelayeret. Denne har vi ferdig definert her.
       const popUpTemplate = new PopupTemplate({
-        title: "{name}",
-        content: [{
-          type: "text",
-          text: `<p>Type: {tourism}</p>
-                  <p>{description}</p>`
-        }]
+        title: '{name}',
+        content: [
+          {
+            type: 'text',
+            text: `<p>Type: {tourism}</p>
+                  <p>{description}</p>`,
+          },
+        ],
       });
 
-      // TODO: Legge hente data
+      // Hent dataen
+      const featureLayer = new FeatureLayer({
+        url: 'https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Tourism_EU/FeatureServer/0',
+        popupEnabled: true,
+        popUpTemplate,
+      });
+      // Legg til dataen i kartet
+      map.add(featureLayer);
+
       // TODO: Legge til dataen i kartet
+      context.featureLayer.set(featureLayer);
+
       // TODO: Legg til dataen i context
 
       // For å kunne vise kartet må dette legges til i et MapView
@@ -54,12 +66,21 @@ const MapComponent = () => {
       // xmin: 10.227928161621094,
       // ymax: 63.453731595863324,
       // xmax: 10.560264587402344
+
       new MapView({
         // MapView trenger minimum feltene:
         // map
+        map,
+        container: mapDiv.current,
+        extent: {
+          ymin: 63.40182257265643,
+          xmin: 10.227928161621094,
+          ymax: 63.453731595863324,
+          xmax: 10.560264587402344,
+        },
         // container
         // extent(valgfritt, men lurt å ha med)
-      }).when(() => {
+      }).when((mapView) => {
         // Når kartet er initialisert kan vi manipulre dataen her
       });
     }
